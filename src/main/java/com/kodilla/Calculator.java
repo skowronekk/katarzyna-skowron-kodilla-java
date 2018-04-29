1613a14c212d192797b7fb49011d0810aba6ac28
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-04-29 09:57:58 +0200

    First Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,27 @@
 package com.kodilla;
 
-public class Calculator {
+import java.lang.*;
+
+public class Calculator
+{
+    public int addMethod(int a, int b) {
+        return a + b;
+    }
+
+    public int substractMethod(int a, int b) {
+        return a - b;
+    }
+}
+
+class Application
+{
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.addMethod(5, 5);
+
+        System.out.println(result);
+
+    }
 }
